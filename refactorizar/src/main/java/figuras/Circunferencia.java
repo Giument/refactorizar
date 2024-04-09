5bd206eb084630265acc1a2b3e1bfa25ac926162
Ignacio <Vespertino@C12PC02.informatica.comercio>
2024-04-09 20:17:54 +0200

    Refactorizar

---
+++ refactorizar/src/main/java/figuras/Circunferencia.java
@@ -0,0 +1,55 @@
+package figuras;
+
+public class Circunferencia {
+
+    /**
+     * @return the radio
+     */
+    public double getRadio() {
+        return radio;
+    }
+
+    /**
+     * @param radio the radio to set
+     */
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+	private double radio;
+
+	public Circunferencia(double radio)
+	    	{
+	        this.radio = radio;
+	}
+
+	    	public void imprimir()
+	    	{
+        color = "rojo";
+	        System.out.println("Di�metro: " + 2*getRadio());
+	        System.out.println("Color: " + color);
+	        double area = 2 * 3.1416 * getRadio() * getRadio();
+	        System.out.println(area);
+	}
+    private String color;
+
+	public boolean esIgual (Circunferencia otro, boolean conDecimales)
+	{
+	        double radio1 = this.getRadio();
+	        double radio2 = otro.getRadio();
+	        if (conDecimales)
+	        {
+	            if (radio1 == radio2) 
+	              return true;
+	            else
+	              return false;
+	        }
+	        else
+	        {
+	            if (Math.abs(radio1-radio2)<1)
+	                return true;
+	            else
+	                return false;
+	        }
+	    }
+	}
+
